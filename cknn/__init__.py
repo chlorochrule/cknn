cdcf6c5225abdaee2fe2aec16e8892fa431e16e1
chlorochrule <minami.polly@gmail.com>
2018-11-26 15:35:28 +0900

    Add brief docstring about cknn

--- cknn/__init__.py
+++ cknn/__init__.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+"""
+The :mod:`cknn` module implements the Continuous k-Nearest Neighbors[1].
+
+Reference
+---------
+.. [1] T. Berry and T. Sauer, “Consistent man-ifold representation for
+       topological dataanalysis,” 2016.
+"""
 
 from .cknn import cknneighbors_graph
 
