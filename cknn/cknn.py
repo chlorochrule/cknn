ad8d09392e8f53f84f777465437206b2a63496ca
chlorochrule <minami.polly@gmail.com>
2018-12-26 21:03:32 +0900

    Fix critical bug

--- cknn/cknn.py
+++ cknn/cknn.py
@@ -181,8 +181,11 @@ class CkNearestNeighbors(object):
             dist = pdist(X, metric=metric)
             dmatrix = squareform(dist)
 
-        darray_n_nbrs = np.partition(dmatrix, n_neighbors)
+        darray_n_nbrs = np.partition(dmatrix, n_neighbors)[:, [n_neighbors]]
         ratio_matrix = dmatrix / np.sqrt(darray_n_nbrs.dot(darray_n_nbrs.T))
+        if verbose == 1:
+            print("ratio matrix is, ")
+            print(ratio_matrix)
         cr_ptr = np.arange(n_samples)
         ratio_matrix[cr_ptr, cr_ptr] = 0
 
