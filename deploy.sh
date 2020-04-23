df396dd4b43eeb6aa5c4beadec3809e5c3dae3b4
chlorochrule <minami.polly@gmail.com>
2020-04-24 01:00:41 +0900

    Debug

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,8 @@ git init
 git config user.email "minami.polly@gmail.com"
 git config user.name "Travis-CI"
 
+[ -z $GITHUB_TOKEN ] && echo GITHUB_TOKEN is empty!!
+
 git add .
 git commit -m "[ci skip] Update docs"
-[ -z $GITHUB_TOKEN ] && echo GITHUB_TOKEN is empty!!
 git push --quit --force https://${GITHUB_TOKEN}@github.com/chlorochrule/cknn.git master:gh-pages >/dev/null 2>&1
