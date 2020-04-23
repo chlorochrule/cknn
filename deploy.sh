24d3a6cafefa2ae3ef24faa772668d9d614683c5
chlorochrule <minami.polly@gmail.com>
2020-04-24 01:42:17 +0900

    Fix

--- deploy.sh
+++ deploy.sh
@@ -16,4 +16,5 @@ git config user.name "Travis-CI"
 
 git add .
 git commit -m "[ci skip] Update docs"
-git push --quit --force https://${GITHUB_TOKEN}@github.com/chlorochrule/cknn.git master:gh-pages >/dev/null 2>&1
+git remote add origin https://${GITHUB_TOKEN}@github.com/chlorochrule/cknn.git
+git push --quit --force origin master:gh-pages >/dev/null 2>&1
