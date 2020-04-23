4aaa733ce03b3a38f250d3898caf6591a910ce74
chlorochrule <minami.polly@gmail.com>
2020-04-24 01:59:03 +0900

    Fix git push option

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,6 @@ git init
 git config user.email "minami.polly@gmail.com"
 git config user.name "Travis-CI"
 
-[ -z $GITHUB_TOKEN ] && echo GITHUB_TOKEN is empty!!
-
 git add .
 git commit -m "[ci skip] Update docs"
-git remote add origin https://${GITHUB_TOKEN}@github.com/chlorochrule/cknn.git
-git push --quit --force origin master:gh-pages >/dev/null 2>&1
+git push --quiet --force https://${GITHUB_TOKEN}@github.com/chlorochrule/cknn.git master:gh-pages >/dev/null 2>&1
